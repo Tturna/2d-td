3679fed7bc4fed46f6149e8f5a56185e58174439
eli <elijah.moody@outlook.com>
2025-08-15 17:27:24 -0400

    basic wave logic

--- src/WaveSystem.cs
+++ src/WaveSystem.cs
@@ -9,6 +9,7 @@ namespace _2d_td
         public struct Formation
         {
             public List<Enemy> enemies;
+            //public int frequency
         }
 
         private static List<Formation> wave;
@@ -16,20 +17,41 @@ namespace _2d_td
         public struct Wave
         {
             public List<Formation> formations;
+            public int formationCooldown;
         }
 
-        public struct Level
+        public struct Zone
         {
             public List<Wave> waves;
-            public int waveNumber;
+            public int currentLvl;
         }
 
-        private static Formation mockForm = new Formation { enemies = new List<Enemy>() };
+        private static Formation mockForm;
+
+        private static Zone zone1;
+
+        private static Wave wave1;
+
+        private float formCooldown;
+
+        private float formCooldownRemaining;
+
+
+        
+
         private static Game1 game;
 
         public static void Initialize(Game1 gameRef)
         {
-            game = gameRef;
+            game = gameRef
+
+            formCooldownTime = 5f;
+
+            mockForm = new Formation { enemies = new List<Enemy>() };
+
+            wave1 = new Wave { formations = new List<Formation>(mockForm) };
+
+            Zone1 = new Zone { waves = new List<Wave>(wave1), currentLvl = 0 }
 
             for (int i = 0; i < 5; i++)
             {
@@ -37,25 +59,45 @@ namespace _2d_td
                 mockForm.enemies.Add(mockEnemy);
                 Console.WriteLine("Added the " + i + "th enemy to the formation");
             }
+
+
         }
 
-        static void SpawnFormation(List<Enemy> formation)
+        public override void Update(GameTime gameTime)
         {
+            float elapsedSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (formCooldownRemaining > 0f) 
+            {
+                formCooldownRemaining -= elapsedSeconds;
+            }
+        }
+
+        static void SpawnFormation(Formation formation)
+        {
+            formCooldownRemaining = formCooldown;
+
             foreach (Enemy enemy in formation)
             {
                 //spawn each enemy, enemy needs spawn function
             }
         }
 
-        static void SpawnWave()
+        static void StartWave(Zone zone)
         {
-            //will call SpawnFormation() per formation in 
+            foreach (Formation formation in zone.waves)
+            {
+                if (formCooldownRemaining < 0f)
+                {
+                    SpawnFormation(formation);
+                }
+            }
         }
 
         static void NextWave()
         {
             //starts next wave
-            //Level.waveNumber++;
+            Zone1.waveNumber++;
         }
     }
 }
