e8306f7fe36b4eba08d7e7d32aa8bbd2fc8c37f9
Tturna <42873350+Tturna@users.noreply.github.com>
2025-08-16 12:59:08 +0300

    animation data constructor, animation texture overriding, enemy hurt frames

--- src/AnimationSystem.cs
+++ src/AnimationSystem.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace _2d_td;
 
+#nullable enable
 public class AnimationSystem
 {
     public struct AnimationData
@@ -12,11 +13,27 @@ public class AnimationSystem
         public int FrameCount;
         public Vector2 FrameSize;
         public float DelaySeconds;
+
+        public AnimationData(Texture2D texture, int frameCount, Vector2 frameSize, float delaySeconds)
+        {
+            Texture = texture;
+            FrameCount = frameCount;
+            FrameSize = frameSize;
+            DelaySeconds = delaySeconds;
+        }
+
+        public AnimationData()
+        {
+            throw new InvalidOperationException("Pass all parameters via constructor!");
+        }
     }
 
     private AnimationData data;
     private float frameTimer;
     private int currentFrame;
+
+    private Texture2D? overrideTexture;
+    private float overrideTimer;
 
     public AnimationSystem(AnimationData animationData)
     {
@@ -26,6 +43,19 @@ public class AnimationSystem
 
     public void UpdateAnimation(float deltaTime)
     {
+        if (overrideTimer > 0f)
+        {
+            overrideTimer -= deltaTime;
+
+            if (overrideTimer <= 0f)
+            {
+                overrideTimer = 0f;
+                overrideTexture = null;
+            }
+
+            return;
+        }
+
         frameTimer -= deltaTime;
 
         if (frameTimer <= 0f)
@@ -35,15 +65,31 @@ public class AnimationSystem
         }
     }
 
+    public void OverrideTexture(Texture2D texture, float durationSeconds)
+    {
+        overrideTexture = texture;
+        overrideTimer = durationSeconds;
+    }
+
     public void Draw(SpriteBatch spriteBatch, Vector2 position, float rotationRadians = 0f,
         Vector2 drawOrigin = default, float drawLayerDepth = 0.9f)
     {
-        var xPosHorizontal = data.FrameSize.X * currentFrame;
-        var x = (int)Math.Floor(xPosHorizontal % data.Texture.Width);
-        var y = (int)(Math.Floor(xPosHorizontal / data.Texture.Width) * data.FrameSize.Y);
-        var sourceRect = new Rectangle(x, y, (int)data.FrameSize.X, (int)data.FrameSize.Y);
+        Rectangle? sourceRect = null;
+        Texture2D texture = data.Texture;
 
-        spriteBatch.Draw(data.Texture,
+        if (overrideTimer > 0f && overrideTexture is not null)
+        {
+            texture = (Texture2D)overrideTexture;
+        }
+        else
+        {
+            var xPosHorizontal = data.FrameSize.X * currentFrame;
+            var x = (int)Math.Floor(xPosHorizontal % data.Texture.Width);
+            var y = (int)(Math.Floor(xPosHorizontal / data.Texture.Width) * data.FrameSize.Y);
+            sourceRect = new Rectangle(x, y, (int)data.FrameSize.X, (int)data.FrameSize.Y);
+        }
+
+        spriteBatch.Draw(texture,
                 position,
                 sourceRectangle: sourceRect,
                 Color.White,
--- src/components/Enemy.cs
+++ src/components/Enemy.cs
@@ -1,24 +1,31 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 // using Microsoft.Xna.Framework.Input;
 
 namespace _2d_td;
 
 public class Enemy : Entity
 {
+    private Texture2D hurtTexture;
+    private float hurtTimeSeconds = 0.1f;
+
     public HealthSystem HealthSystem;
     public PhysicsSystem PhysicsSystem;
     public MovementSystem MovementSystem;
     public AnimationSystem AnimationSystem;
 
     public Enemy(Game game, Vector2 position, Vector2 size, MovementSystem.MovementData movementData,
-        AnimationSystem.AnimationData animationData) : base(game, position, size: size)
+        AnimationSystem.AnimationData animationData, Texture2D hurtTexture) : base(game, position, size: size)
     {
         HealthSystem = new HealthSystem(owner: this, initialHealth: 100);
         HealthSystem.Died += OnDeath;
+        HealthSystem.Damaged += OnDamaged;
 
         PhysicsSystem = new PhysicsSystem(Game);
         MovementSystem = new MovementSystem(Game, movementData);
         AnimationSystem = new AnimationSystem(animationData);
+
+        this.hurtTexture = hurtTexture;
     }
 
     public override void Update(GameTime gameTime)
@@ -53,6 +60,11 @@ public class Enemy : Entity
     public override void Draw(GameTime gameTime)
     {
         AnimationSystem.Draw(Game.SpriteBatch, Position, RotationRadians, DrawOrigin, DrawLayerDepth);
+    }
+
+    private void OnDamaged(Entity damagedEntity)
+    {
+        AnimationSystem.OverrideTexture(hurtTexture, hurtTimeSeconds);
     }
 
     private void OnDeath(Entity diedEntity)
