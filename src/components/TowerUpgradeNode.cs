410911dc5ca34d8df11eb7c61233ff76350a62f8
Tturna <42873350+Tturna@users.noreply.github.com>
2025-09-12 15:05:01 +0300

    tower upgrade system, most of gun turret upgrade mechanics

---
+++ src/components/TowerUpgradeNode.cs
@@ -0,0 +1,24 @@
+namespace _2d_td;
+
+#nullable enable
+public class TowerUpgradeNode
+{
+    public string Name { get; private set; }
+    public TowerUpgradeNode? Parent { get; private set; }
+    public TowerUpgradeNode? LeftChild { get; private set; }
+    public TowerUpgradeNode? RightChild { get; private set; }
+
+    public TowerUpgradeNode(string name, TowerUpgradeNode? parent = null,
+        TowerUpgradeNode? leftChild = null, TowerUpgradeNode? rightChild = null)
+    {
+        Name = name;
+        Parent = parent;
+        LeftChild = leftChild;
+        RightChild = rightChild;
+    }
+
+    public void SetParent(TowerUpgradeNode parent)
+    {
+        Parent = parent;
+    }
+}
