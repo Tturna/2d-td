46abff909891ab49e7f98ec6ef73fb36a9b1dc25
skahds <skahd.dev@gmail.com>
2025-09-14 16:19:56 +0700

    railgun

--- src/components/turrets/Railgun.cs
+++ src/components/turrets/Railgun.cs
@@ -4,11 +4,14 @@ namespace _2d_td;
 
 // TODO: Abstract some of this functionality so that methods don't repeat a million times
 // across different turret implementations.
+#nullable enable
 class Railgun : Entity
 {
     private TowerCore towerCore;
+    private Vector2 spawnOffset = new (0, 3);
     int tileRange = 18;
     int damage = 30;
+    float bulletSpeed = 900f;
     float actionsPerSecond = 0.5f;
     float actionTimer;
 
@@ -28,22 +31,35 @@ class Railgun : Entity
 
         actionTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-        if (actionTimer >= actionInterval)
+        if (actionTimer >= actionInterval && IsEnemyInLine(tileRange))
         {
-            ShootAtClosestEnemy();
+            Shoot();
             actionTimer = 0f;
         }
 
         base.Update(gameTime);
     }
 
-    private void ShootAtClosestEnemy()
+    private void Shoot()
     {
-        var closestEnemy = towerCore.GetClosestEnemy(tileRange);
+        var direction = new Vector2(-1, 0);
+        var bullet = new Projectile(Game, Position+spawnOffset, direction, damage, bulletSpeed, 2);
+        bullet.bulletLength = 30f;
+        Game.Components.Add(bullet);
+    }
 
-        if (closestEnemy is null) return;
-
-        closestEnemy.HealthSystem.TakeDamage(damage);
+    public bool IsEnemyInLine(int tileRange)
+    {
+        // TODO: Don't loop over all enemies. Just the ones in range.
+        foreach (Enemy enemy in EnemySystem.Enemies)
+        {
+            if (enemy.Position.Y < Position.Y + Size.Y &&
+                enemy.Position.Y > Position.Y)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     public override void Destroy()
