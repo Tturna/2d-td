5d5d5b6960a4f705ed8c84604bc95df9cad3527b
Tturna <42873350+Tturna@users.noreply.github.com>
2025-12-02 12:13:28 +0200

    clean ITower function summary

--- src/interfaces/ITower.cs
+++ src/interfaces/ITower.cs
@@ -9,8 +9,9 @@ public interface ITower
     /// <summary>
     /// Returns a key-value pair for each piece of a tower, including the base. Keys are
     /// the UI entities with the sprite of a certain piece. Values are relative positions
-    /// to the base. The base should be the first element in the list with relative position (0, 0).
-    /// </summary
+    /// to the base. The base should be the first element in the list. The base element should
+    /// have relative position (0, 0) unless it needs an offset.
+    /// </summary>
     public abstract static List<KeyValuePair<UIEntity, Vector2>> GetUnupgradedPartIcons(List<UIEntity> uiElements);
     public abstract static Vector2 GetDefaultGridSize();
     public abstract static BuildingSystem.TowerType GetTowerType();
